015affaa306506dadf7035b00b193211201dcfe7
FLI\nbaynham <nbaynham@flhi.com>
2019-10-17 18:46:02 -0400

    added testng

---
+++ src/test/java/io/nbaynham/examples/TestNGExample.java
@@ -0,0 +1,11 @@
+package io.nbaynham.examples;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class TestNGExample {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
